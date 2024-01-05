830ad1d7b283040f36e3b9e04259effe8a856958
karapeprajakta <karapeprajakta1@gmail.com>
2024-01-05 23:27:16 +0530

    added

---
+++ server.js
@@ -0,0 +1,21 @@
+var express =require('express');
+var app=express();
+app.use(express.static('public'));
+app.get("/",(req,res)=>{
+    console.log("Welcome to our server");
+});
+app.get("/product",(req,res)=>{
+
+    var p={
+        "id":45,
+        "title":"Gerbera",
+         "discription":"wedding flower",
+
+        " price":500,
+
+       "qyantity":800
+        }
+});
+res.send(p);
+app.listen(8000);
+console.log("server is running on port 8000");
